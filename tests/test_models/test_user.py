8ed1f05d42c26880b5eae174060a059d503a3ede
LameckL <88710250+LameckL@users.noreply.github.com>
2023-12-19 14:37:34 +0000

    test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" """
+""" test"""
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
 
 class test_User(test_basemodel):
-    """ """
+    """ test class for user"""
 
     def __init__(self, *args, **kwargs):
         """ """
