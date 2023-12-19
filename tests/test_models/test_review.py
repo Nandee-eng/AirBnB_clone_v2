ca09095857b234b8958e84c946a255010995b36a
LameckL <88710250+LameckL@users.noreply.github.com>
2023-12-19 14:36:28 +0000

    test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" """
+""" test"""
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """ test class for review"""
 
     def __init__(self, *args, **kwargs):
         """ """
